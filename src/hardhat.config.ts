bacd5652ec967f4007c66026b1ac7f2e663ac174
Oleg <martelskiy@gmail.com>
2024-04-25 16:14:54 +0200

    Add few integration tests and deployment to Sepolia network

--- src/hardhat.config.ts
+++ src/hardhat.config.ts
@@ -3,6 +3,9 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
 import "dotenv-defaults/config";
 
+const SEPOLIA_DEPLOYER_PK = process.env.SEPOLIA_DEPLOYER_PK;
+const SEPOLIA_ALCHEMY_API_KEY = process.env.SEPOLIA_ALCHEMY_API_KEY;
+
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [{ version: "0.8.23" }],
@@ -13,6 +16,10 @@ const config: HardhatUserConfig = {
         enabled: false,
         url: ``,
       },
+    },
+    sepolia: {
+      accounts: [SEPOLIA_DEPLOYER_PK!],
+      url: `https://eth-sepolia.g.alchemy.com/v2/${SEPOLIA_ALCHEMY_API_KEY}`,
     },
   },
 };
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -2,6 +2,10 @@ import { ethers, network } from "hardhat";
 import { StakeERC20 } from "../typechain-types";
 import { deploy } from "../utils/contract";
 
+const stakedToken: ReadonlyMap<string, string> = new Map<string, string>([
+  ["sepolia", "0x51fCe89b9f6D4c530698f181167043e1bB4abf89"],
+]);
+
 async function main() {
   const [signer] = await ethers.getSigners();
 
@@ -10,7 +14,7 @@ async function main() {
      with owner set to: '${signer.address}'`,
   );
 
-  await deploy<StakeERC20>("StakeERC20");
+  await deploy<StakeERC20>("StakeERC20", stakedToken.get(network.name));
 }
 
 main().catch((error) => {

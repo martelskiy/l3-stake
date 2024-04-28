f996d57255db7614ea263cb3b9e67a14f3d6047d
Oleg <martelskiy@gmail.com>
2024-04-28 13:15:05 +0200

    Add deployment configuration for Base network

--- src/hardhat.config.ts
+++ src/hardhat.config.ts
@@ -6,6 +6,9 @@ import "dotenv-defaults/config";
 const SEPOLIA_DEPLOYER_PK = process.env.SEPOLIA_DEPLOYER_PK;
 const SEPOLIA_ALCHEMY_API_KEY = process.env.SEPOLIA_ALCHEMY_API_KEY;
 
+const BASE_DEPLOYER_PK = process.env.BASE_DEPLOYER_PK;
+const BASE_ALCHEMY_API_KEY = process.env.BASE_ALCHEMY_API_KEY;
+
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [{ version: "0.8.23" }],
@@ -20,6 +23,10 @@ const config: HardhatUserConfig = {
     sepolia: {
       accounts: [SEPOLIA_DEPLOYER_PK!],
       url: `https://eth-sepolia.g.alchemy.com/v2/${SEPOLIA_ALCHEMY_API_KEY}`,
+    },
+    base: {
+      accounts: [BASE_DEPLOYER_PK!],
+      url: `https://base-mainnet.g.alchemy.com/v2/${BASE_ALCHEMY_API_KEY}`,
     },
   },
 };
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -4,6 +4,7 @@ import { deploy } from "../utils/contract";
 
 const stakedToken: ReadonlyMap<string, string> = new Map<string, string>([
   ["sepolia", "0x51fCe89b9f6D4c530698f181167043e1bB4abf89"],
+  ["base", "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"],
 ]);
 
 async function main() {

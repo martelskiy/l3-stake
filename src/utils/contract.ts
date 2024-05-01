5def95bcd26211b988dabd22168f137cbc79926f
Oleg <martelskiy@gmail.com>
2024-05-01 13:42:12 +0200

    Add upgrades support to deployment scripts

--- src/utils/contract.ts
+++ src/utils/contract.ts
@@ -1,21 +1,27 @@
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
 
 export async function deploy<BaseContract>(
+  withProxy: boolean,
   name: string,
   ...ctorArguments: any
 ): Promise<BaseContract> {
   console.log(
-    `deploying ${name} contract with constructor arguments: ${ctorArguments}`
+    `deploying ${name} contract with constructor arguments: ${ctorArguments}`,
   );
 
   const contractFactory = await ethers.getContractFactory(name);
-  const contract = await contractFactory.deploy(...ctorArguments);
+  let contract;
+  if (withProxy) {
+    contract = await upgrades.deployProxy(contractFactory, ctorArguments);
+  } else {
+    contract = await contractFactory.deploy(...ctorArguments);
+  }
 
   const deployedContract = (await contract.waitForDeployment()) as BaseContract;
 
   const address = await contract.getAddress();
   console.log(
-    `contract with name: '${name}' was deployed to address: '${address}'`
+    `contract with name: '${name}' was deployed to address: '${address}'`,
   );
 
   return deployedContract;

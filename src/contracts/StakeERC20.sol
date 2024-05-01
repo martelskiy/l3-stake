669d47f3a9665a6af2f90842c44826cfb4172edf
Oleg <martelskiy@gmail.com>
2024-05-01 13:37:50 +0200

    Make Stake contract upgradaeble, fix bug with double stakes, update tests

--- src/contracts/StakeERC20.sol
+++ src/contracts/StakeERC20.sol
@@ -2,8 +2,11 @@
 pragma solidity ^0.8.0;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract StakeERC20 {
+contract StakeERC20 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     IERC20 public stakingToken;
     uint256 public constant REWARD_RATE = 1e18; // 100%
     uint256 public constant LOCK_PERIOD = 7 days;
@@ -19,26 +22,30 @@ contract StakeERC20 {
     error TransferFailed();
     error TokensLocked();
     error NotEnoughTimePassed();
+    error AddressHasStake(address staker);
 
     event Staked(address indexed user, uint256 amount);
     event Unstaked(address indexed user, uint256 amount, uint256 reward);
     event RewardClaimed(address indexed user, uint256 reward);
 
-    constructor(address _token) {
+    function initialize(address _token) public initializer {
         stakingToken = IERC20(_token);
+
+        __Ownable_init(_msgSender());
     }
 
     function stake(uint256 _amount) external {
         if (_amount == 0) revert ZeroAmountStaked();
-        if (!stakingToken.transferFrom(msg.sender, address(this), _amount))
+        if (hasStake(_msgSender())) revert AddressHasStake(_msgSender());
+        if (!stakingToken.transferFrom(_msgSender(), address(this), _amount))
             revert TransferFailed();
 
-        stakes[msg.sender] = Stake({
+        stakes[_msgSender()] = Stake({
             amount: _amount,
             startTime: block.timestamp
         });
 
-        emit Staked(msg.sender, _amount);
+        emit Staked(_msgSender(), _amount);
     }
 
     function claimReward() external {
@@ -80,4 +87,10 @@ contract StakeERC20 {
         uint256 rewardRatePerSecond = REWARD_RATE / 365 days;
         return (_amount * rewardRatePerSecond * _stakeDuration) / 1e18;
     }
+
+    function hasStake(address addr) public view returns (bool) {
+        return stakes[addr].startTime != 0;
+    }
+
+    function _authorizeUpgrade(address) internal override onlyOwner {}
 }
